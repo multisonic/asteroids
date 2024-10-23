297316d9b02287bb1f32e9324569f3ac394a9162
Michael Zaleski <me@mikezaleski.com>
2024-10-23 09:53:35 -0400

    2.7 Moving - Official Solution

--- player.py
+++ player.py
@@ -22,24 +22,18 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a]:
-            self.rotate(-dt)
-        if keys[pygame.K_d]:
-            self.rotate(dt)
         if keys[pygame.K_w]:
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def move(self, dt):
-        # We start with a unit vector pointing straight up from (0, 0) to (0, 1).
-        # We rotate that vector by the player's rotation, so it's pointing in the direction the player is facing.
-        # We multiply by PLAYER_SPEED * dt. A larger vector means faster movement.
-        # Add the vector to our position to move the player.
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
-        
-  
