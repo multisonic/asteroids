a0ecbd0e1975087eec9128bae0e77cabdd380f67
Michael Zaleski <me@mikezaleski.com>
2024-10-25 00:50:02 -0400

    2.8 Groups - Official Solution

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+
 
 def main():
-	pygame.init()
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	clock = pygame.time.Clock()
-	updatable = pygame.sprite.Group()
-	drawable = pygame.sprite.Group()
-	Player.containers = (updatable, drawable)
-	newplayer = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
-	dt = 0
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
+    Player.containers = (updatable, drawable)
 
-	#GAME LOOP
-	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				return
-		
-		# newplayer.update(dt)
-		for player in updatable:
-			player.update(dt)
-	
-		screen.fill("black")
-		for player in drawable:
-			player.draw(screen)
-		# newplayer.draw(screen)
-		pygame.display.flip()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-		# limit framerate to 60 FPS
-		dt = clock.tick(60) / 1000
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
